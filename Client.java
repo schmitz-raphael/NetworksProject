ff7c1c9bd22678e6da403d8a640edfe0f882611d
Raphael Schmitz <raphael.schmitz.002@student.uni.lu>
2024-01-03 19:48:03 +0100

    Sending files now works properly + to multiple users

--- Client.java
+++ Client.java
@@ -1,156 +1,103 @@
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.*;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class Client {
     private DatagramSocket socket;
     private InetSocketAddress serverAddress;
     private String filename;
-    private int windowSize;
-    private double errorProbability;
-    private Timer timer;
-    private int base;
-    private int nextSeqNum;
-    private byte[][] packets;
-    private boolean[] ackReceived;
+    private int packetsReceived = 0;
+    private int id;
 
-    public Client(InetSocketAddress serverAddress, String filename, int windowSize, double errorProbability) throws SocketException {
+    public Client(InetSocketAddress serverAddress, String filename, int id) throws SocketException {
         this.socket = new DatagramSocket();
         this.serverAddress = serverAddress;
         this.filename = filename;
-        this.windowSize = windowSize;
-        this.errorProbability = errorProbability;
-        this.timer = new Timer();
-        this.base = 0;
-        this.nextSeqNum = 0;
-        this.packets = new byte[windowSize][];
-        this.ackReceived = new boolean[windowSize];
-
-        // Initialize the packets array with data from the file
-        try (BufferedInputStream fileInputStream = new BufferedInputStream(new FileInputStream(filename))) {
-            for (int i = 0; i < windowSize; i++) {
-                byte[] data = new byte[1024];
-                int bytesRead = fileInputStream.read(data);
-                if (bytesRead == -1) {
-                    break; // End of file
-                }
-                packets[i] = new byte[bytesRead];
-                System.arraycopy(data, 0, packets[i], 0, bytesRead);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        this.id = id;
     }
 
     public void join() {
         try {
             // Send join request to the server
-            byte[] joinData = "JOIN".getBytes();
-            DatagramPacket joinPacket = new DatagramPacket(joinData, joinData.length, serverAddress.getAddress(),
-                    serverAddress.getPort());
-            socket.send(joinPacket);
-
-            // Wait for acknowledgment from the server
-            byte[] ackData = new byte[1024];
-            DatagramPacket ackPacket = new DatagramPacket(ackData, ackData.length);
-            socket.receive(ackPacket);
-
+            sendPacket(("JOIN " + filename).getBytes());
+            waitForAck();
             System.out.println("Joined server: " + serverAddress);
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    public void start() {
-        // Start a timer task to handle retransmissions
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                handleTimeout();
-            }
-        }, 1000, 1000); // Adjust the delay and period as needed
-
-        // Send the initial window of packets
-        for (int i = 0; i < windowSize; i++) {
-            sendPacket(i);
-        }
-
-        // Continue sending packets until all have been acknowledged
-        while (base < packets.length) {
-            // Wait for acknowledgments
-            waitForAcks();
-
-            // Send the next packet in the window
-            sendPacket(nextSeqNum);
-        }
-
-        // Stop the timer task
-        timer.cancel();
-        timer.purge();
-
-        System.out.println("File transfer complete.");
-    }
-
-
-    private void sendWithSimulation(byte[] data) {
-        if (Math.random() > errorProbability) {
-            try {
-                DatagramPacket packet = new DatagramPacket(data, data.length, serverAddress.getAddress(),
-                        serverAddress.getPort());
-                socket.send(packet);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            System.out.println("Packet lost. Simulating error.");
-        }
-    }
-    private void handleTimeout() {
-        for (int i = base; i < nextSeqNum; i++) {
-            if (!ackReceived[i % windowSize]) {
-                // Timeout occurred for this packet, retransmit
-                sendPacket(i);
-            }
-        }
-    }
-    private void waitForAcks() {
-        while (true) {
-            if (ackReceived[base % windowSize]) {
-                // Acknowledgment received for the base packet, move the window
-                base++;
-            }
-            // Check if all packets in the window have been acknowledged
-            if (base == nextSeqNum) {
+    public void waitForAck(){
+        while (true){
+            DatagramPacket ackPacket = receivePacket();
+            if (ackPacket.getLength() > 0 && new String(ackPacket.getData()).trim().equals("ACK")) {
+                System.out.println("Acknowledgment received.");
                 break;
             }
         }
     }
-    private void sendPacket(int seqNum) {
-        if (seqNum < packets.length) {
-            sendWithSimulation(packets[seqNum]);
-            nextSeqNum++;
+    public void receiveFile() {
+        try (BufferedOutputStream fileOutputStream = new BufferedOutputStream(new FileOutputStream(id +"_incoming_" + filename))) {
+            while (true) {
+                DatagramPacket receivePacket = receivePacket();
+
+                if (receivePacket.getLength() == 0 || isEndOfFilePacket(receivePacket)) {
+                    break; // End of file transfer
+                }
+
+                fileOutputStream.write(receivePacket.getData(), 0, receivePacket.getLength());
+                System.out.println("Packet " + packetsReceived + " received.");
+
+                // Send acknowledgment
+                sendAck();
+            }
+            fileOutputStream.close();
+            System.out.println("File transfer complete.");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
+    private void sendPacket(byte[] data) throws IOException {
+        if (Math.random() > 0.00){
+            DatagramPacket packet = new DatagramPacket(data, data.length, serverAddress.getAddress(), serverAddress.getPort());
+            socket.send(packet);
+        }
+        else System.out.println("Packet lost");
+    }
+
+    private DatagramPacket receivePacket() {
+        try {
+            byte[] receiveData = new byte[2056];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            socket.receive(receivePacket);
+            return receivePacket;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void sendAck() throws IOException {
+        String ackMessage = "ACK " + (packetsReceived++);
+        sendPacket(ackMessage.getBytes());
+    }
+    private boolean isEndOfFilePacket(DatagramPacket packet) {
+        String packetData = new String(packet.getData(), 0, packet.getLength()).trim();
+        return packetData.equals("END_OF_FILE");
+    }
     public static void main(String[] args) {
         try {
-            String serverHost = args[0];
-            int serverPort = Integer.parseInt(args[1]);
-            String filename = args[2];
-            int windowSize = Integer.parseInt(args[3]);
-            double errorProbability = Double.parseDouble(args[4]);
 
-            InetSocketAddress serverAddress = new InetSocketAddress(serverHost, serverPort);
+            String filename = args[0];
+            int id = Integer.parseInt(args[1]);
+            InetSocketAddress serverAddress = new InetSocketAddress("localhost", 6666);
 
-            Client client = new Client(serverAddress, filename, windowSize, errorProbability);
+            Client client = new Client(serverAddress, filename, id);
             client.join();
-            client.start();
+            client.receiveFile();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
 }
--- Server.java
+++ Server.java
@@ -1,102 +1,117 @@
+
+
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.*;
 import java.util.*;
 
 public class Server {
     private DatagramSocket socket;
-    private int totalProcesses;
-    private int windowSize;
-    private String filename;
-    private double errorProbability;
-    private List<InetSocketAddress> clients;
-    private Map<InetSocketAddress, Integer> lastAckReceived;
+    private int port;
+    private int ackCounter = 0;
+    private ArrayList<String> fileNames = new ArrayList<>();
+    private ArrayList<Integer> clients = new ArrayList<>();
 
-    public Server(int port, int totalProcesses, int windowSize, String filename, double errorProbability) throws SocketException {
+    public Server() throws SocketException {
+        this.port = 6666;
         this.socket = new DatagramSocket(port);
-        this.totalProcesses = totalProcesses;
-        this.windowSize = windowSize;
-        this.filename = filename;
-        this.errorProbability = errorProbability;
-        this.clients = new ArrayList<>();
-        this.lastAckReceived = new HashMap<>();
     }
 
     public void start() {
-        System.out.println("Server started. Waiting for clients to join...");
-
-        // Wait for all clients to join
-        while (clients.size() < totalProcesses) {
-            receiveJoinRequest();
-        }
-
-        System.out.println("All clients joined. Initiating file transfer...");
-
-        // Start file transfer
-        sendFile();
-    }
-
-    private void receiveJoinRequest() {
         try {
-            byte[] receiveData = new byte[1024];
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            socket.receive(receivePacket);
+            System.out.println("Server started on port " + port);
 
-            InetSocketAddress clientAddress = new InetSocketAddress(receivePacket.getAddress(), receivePacket.getPort());
+            // Wait for the client to join
+            waitForJoin();
 
-            if (!clients.contains(clientAddress)) {
-                clients.add(clientAddress);
-                System.out.println("Client " + clients.size() + " joined: " + clientAddress);
-            }
+            // Send the file to the client
+            sendFile();
 
-            // Send acknowledgment
-            sendAck(clientAddress, 0);
-        } catch (Exception e) {
+            System.out.println("File transfer complete.");
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void sendAck(InetSocketAddress clientAddress, int sequenceNumber) {
-        try {
-            DatagramPacket ackPacket = new DatagramPacket(String.valueOf(sequenceNumber).getBytes(),
-                    String.valueOf(sequenceNumber).length(), clientAddress.getAddress(), clientAddress.getPort());
-            socket.send(ackPacket);
-        } catch (Exception e) {
-            e.printStackTrace();
+    private void waitForJoin() throws IOException {
+        while (clients.size() < 2) {
+            DatagramPacket joinPacket = receivePacket();
+            String joinMessage = new String(joinPacket.getData(), 0, joinPacket.getLength());
+            if (joinPacket.getLength() > 0 && joinMessage.split(" ")[0].equals("JOIN")) {
+                System.out.println("Client joined: " + joinPacket.getAddress() + ":" + joinPacket.getPort());
+                fileNames.add(joinMessage.split(" ")[1]);
+                clients.add(joinPacket.getPort());
+                sendAck(joinPacket.getAddress(), joinPacket.getPort());
+                
+            }
+        }
+    }
+    
+    public void sendFile() throws IOException {
+        for (int i = 0; i < clients.size(); i++){
+            ackCounter = 0;
+            try (BufferedInputStream fileInputStream = new BufferedInputStream(new FileInputStream(fileNames.get(i)))) {
+                byte[] buffer = new byte[2048];
+                int bytesRead;
+
+                while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                    sendPacket(buffer, bytesRead, clients.get(i));
+                    waitForAck();
+                }
+
+                // Signal end of file transfer
+                byte[] endOfFileMessage = "END_OF_FILE".getBytes();
+                DatagramPacket endOfFilePacket = new DatagramPacket(endOfFileMessage, endOfFileMessage.length, InetAddress.getLocalHost(), clients.get(i));
+                socket.send(endOfFilePacket);
+            }
+        }
+    }
+    public void sendAck(InetAddress address, int clientPort) throws IOException {
+        sendPacket("ACK".getBytes(), address, clientPort);
+    }
+    private void sendPacket(byte[] data, InetAddress address, int port) throws IOException {
+        DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
+        socket.send(packet);
+    }
+    public void sendPacket(byte[] data, int length, int clientPort) throws IOException {
+        if (Math.random() > 0.00){
+            DatagramPacket packet = new DatagramPacket(data, length, InetAddress.getLocalHost(), clientPort);
+            socket.send(packet);
+            System.out.println(": Packet sent");
+        }
+        else{
+            System.out.println("Packet lost");
         }
     }
 
-    private void sendFile() {
-        // Implement file transfer logic here using Go-Back-N protocol
-        // You need to handle window management, timers, retransmissions, etc.
-        // Use the UDP send wrapper function with error simulation
-        // Update lastAckReceived map when acknowledgments are received
+    private DatagramPacket receivePacket() throws IOException {
+        byte[] receiveData = new byte[2056];
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        socket.receive(receivePacket);
+        return receivePacket;
     }
 
-    private void sendWithSimulation(InetSocketAddress clientAddress, byte[] data) {
-        if (Math.random() > errorProbability) {
-            try {
-                DatagramPacket packet = new DatagramPacket(data, data.length, clientAddress.getAddress(),
-                        clientAddress.getPort());
-                socket.send(packet);
-            } catch (Exception e) {
-                e.printStackTrace();
+    private void waitForAck() throws IOException {
+        while (true){
+            DatagramPacket ackPacket = receivePacket();
+            String ackMessage = new String(ackPacket.getData(), 0, ackPacket.getLength());
+            if (ackMessage.split(" ")[0].equals("ACK") && Integer.parseInt(ackMessage.split(" ")[1]) == ackCounter) {
+                System.out.println("Acknowledgment " + ackCounter++ + " received" );
+                break;
             }
-        } else {
-            System.out.println("Packet lost. Simulating error.");
         }
     }
 
     public static void main(String[] args) {
-        try {
-            int port = Integer.parseInt(args[0]);
-            int totalProcesses = Integer.parseInt(args[1]);
-            int windowSize = Integer.parseInt(args[2]);
-            String filename = args[3];
-            double errorProbability = Double.parseDouble(args[4]);
-
-            Server server = new Server(port, totalProcesses, windowSize, filename, errorProbability);
+        try{
+            Server server = new Server();
             server.start();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
+
+
